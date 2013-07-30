c67cd29ae29b2f74755195964b16bf3a0e809448
pschow <paulschow@gmail.com>
2013-07-30 19:42:09 +0000

    added comments

--- mcp3208.py
+++ mcp3208.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# mcp3008_lm35.py - read an LM35 on CH0 of an MCP3008 on a Raspberry Pi
+# mcp3208_lm35.py - read an 1k Themistisor on CH0 of an MCP3208 on a Raspberry Pi
 # mostly nicked from
 #  http://jeremyblythe.blogspot.ca/2012/09/raspberry-pi-hardware-spi-analog-inputs.html
+# Also from
+# http://scruss.com/blog/2013/02/02/simple-adc-with-the-raspberry-pi/
  
 import spidev
 import time
@@ -11,11 +13,11 @@ import math
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
-f = open('ttest1.txt', 'w')
-print f
+log = open('ttest1.txt', 'w') #open a text file for logging
+print log #print what the log file is
  
 def readadc(adcnum):
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+# read SPI data from MCP3208 chip, 8 possible adc's (0 thru 7)
     if adcnum > 7 or adcnum < 0:
         return -1
     r = spi.xfer2([1, 8 + adcnum << 4, 0])
@@ -23,21 +25,31 @@ def readadc(adcnum):
     return adcout
  
 while True:
-    value = readadc(0)
-    volts = (value * 3.3) / 1024
-    ohms = ((1/volts)*3300)-1000
-    lnohm = math.log1p(ohms)
+    value = readadc(0) #read the adc
+    volts = (value * 3.3) / 1024 #calculate the voltage
+    ohms = ((1/volts)*3300)-1000 #calculate the ohms of the thermististor
+
+    lnohm = math.log1p(ohms) #take ln(ohms)
+
+    #a, b, & c values from http://www.thermistor.com/calculators.php
+    #using curve R (-6.2%/C @ 25C) Mil Ratio X
     a =  0.002197222470870
     b =  0.000161097632222
-    t1 = (b*lnohm) 
     c =  0.000000125008328
-    c2 = c*lnohm
-    #c2 = math.pow(8.587749408080068,-7)
-    t2 = math.pow(c2,3)
-    temp = 1/(a + t1 + t2)
-    tempc = temp - 273.15 - 4
-    print ("%4d/1023 => %5.3f V => %4.1f Ω => %4.1f °K => %4.1f °C" % (value, volts,
-            ohms, temp, tempc))
-    f.write('%f\n' % (tempc))
-    #print ("%f" % c2)
-    time.sleep(10)
+
+    #Steinhart Hart Equation
+    # T = 1/(a + b[ln(ohm)] + c[ln(ohm)]^3)
+
+    t1 = (b*lnohm) # b[ln(ohm)]
+
+    c2 = c*lnohm # c[ln(ohm)]
+
+    t2 = math.pow(c2,3) # c[ln(ohm)]^3
+
+    temp = 1/(a + t1 + t2) #calcualte temperature
+
+    tempc = temp - 273.15 - 4 #K to C
+    #print out info
+    print ("%4d/1023 => %5.3f V => %4.1f Ω => %4.1f °K => %4.1f °C" % (value, volts, ohms, temp, tempc))
+    log.write('%f\n' % (tempc)) #write to log
+    time.sleep(10) #wait 10 seconds

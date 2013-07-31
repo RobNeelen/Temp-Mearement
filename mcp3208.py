dd5b62a458a11825cad3f32e3bf3233899318355
pschow <paulschow@gmail.com>
2013-07-31 00:26:02 +0000

    added multiple sensor reading

--- mcp3208.py
+++ mcp3208.py
@@ -34,16 +34,18 @@ def readadc(adcnum):
 
 class index:
     def GET(self):
-        getInput = web.input(turn="")
-        command = str(getInput.turn)
-        if command == "on":
-            return render.index(round(temp_get(),1))
+        getInput = web.input(adc="")
+        command = str(getInput.adc)
+        #if command == "on":
+        adc = int(command)
+        return render.index(round(temp_get(adc),1))
 
 if __name__ == "__main__":
         app.run()
 
-def temp_get():
-    value = readadc(0) #read the adc
+#thermistor reading function
+def temp_get(adc):
+    value = readadc(adc) #read the adc
     volts = (value * 3.3) / 1024 #calculate the voltage
     ohms = ((1/volts)*3300)-1000 #calculate the ohms of the thermististor
 
@@ -67,8 +69,10 @@ def temp_get():
     temp = 1/(a + t1 + t2) #calcualte temperature
 
     tempc = temp - 273.15 - 4 #K to C
+    # the -4 is error correction for bad python math
+
     #print out info
-    print ("%4d/1023 => %5.3f V => %4.1f Ω => %4.1f °K => %4.1f °C" % (value, volts, ohms, temp, tempc))
+    print ("%4d/1023 => %5.3f V => %4.1f Ω => %4.1f °K => %4.1f °C from adc %d" % (value, volts, ohms, temp, tempc, adc))
     log.write('%f\n' % (tempc)) #write to log
     #time.sleep(10) #wait 10 seconds
     return tempc
